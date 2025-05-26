15ef6edbb3ad0d6d1f2f92e4f823befbbaed479e
oldground <mulmanmyong@gmail.com>
2025-05-26 21:53:46 +0900

    1780.cpp solve

--- boj/cpp_boj/SILVER/1780.cpp
+++ boj/cpp_boj/SILVER/1780.cpp
@@ -4,8 +4,42 @@
 
 using namespace std;
 
+int n;
+int arr[2188][2188];
+int cnt[3]; // -1 0 1
+
+bool check(int a, int b, int n) {
+  for (int i = a; i < a+n; i++) {
+    for (int j = b; j < b+n; j++) {
+      if (arr[a][b] != arr[i][j]) return false;
+    }
+  }
+  return true;
+}
+
+void paper(int a, int b, int n) {
+  if (check(a, b, n)) {
+    cnt[arr[a][b] + 1] += 1;
+    return;
+  }
+  int tmp = n / 3;
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      paper(a+tmp*i, b+tmp*j, tmp);
+    }
+  }
+}
+
 void func1(void) {
-  
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      cin >> arr[i][j];
+    }
+  }
+  paper(0, 0, n);
+
+  for (int ans : cnt) cout << ans << '\n';
 }
 
 int main(void)
