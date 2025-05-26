0ceda97da7820be2d574ce9e17d8f484fffd0f0c
oldground <mulmanmyong@gmail.com>
2025-05-26 16:22:14 +0900

    add 1780.cpp

---
+++ boj/cpp_boj/SILVER/1780.cpp
@@ -0,0 +1,17 @@
+// 1780. 종이의 개수 -> 실버 2
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void func1(void) {
+  
+}
+
+int main(void)
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  func1();
+  return 0;
+}
