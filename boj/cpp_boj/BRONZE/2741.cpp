97de06bc2b89f840825ddbb8d63a91a5a5839b78
oldground <mulmanmyong@gmail.com>
2025-06-07 16:05:34 +0900

    2741.cpp solve

---
+++ boj/cpp_boj/BRONZE/2741.cpp
@@ -0,0 +1,17 @@
+// 2741. N 찍기 -> 브론즈 5
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void func1(void) {
+  int n;
+  cin >> n;
+  for (int i = 1; i <= n; i++)  cout << i << '\n';
+}
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  func1();
+  return 0;
+}
