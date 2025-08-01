49ae3c9d711b3da434bf88181978a875d464b90b
oldground <mulmanmyong@gmail.com>
2025-08-02 00:43:20 +0900

    Main6131.java solve

---
+++ boj/java_boj/src/Main6131.java
@@ -0,0 +1,26 @@
+// 6131. 완전 제곱수 -> 브론즈 3
+
+import java.util.Scanner;
+
+public class Main6131 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt(); //A의 제곱과 B의 제곱의 간격 
+		
+		int ans = 0; // 정답 count 
+		//A >= B이고, 1 ~ 500까지
+		for (int numB = 1; numB <= 500; numB++ ) {
+			// A는 B이상 
+			for (int numA = numB; numA <= 500; numA++) {
+				if (numA*numA == numB*numB + N) {
+					// A의 제곱은 B의 제곱보다 N만큼 크다.
+					// 단순 완전탐색으로 구하기 
+					ans++; 
+				}
+			}
+		}
+		
+		System.out.println(ans);
+	}
+}
