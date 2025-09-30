2717b9359c5e26570efcf53a261c6188c11b8da9
mulmanmyong <mulmanmyong@gmail.com>
2025-09-30 23:21:02 +0900

    BOJ 9086 문자열 해결(c++)

---
+++ boj/cpp_boj/BRONZE/BOJ_9086_문자열_1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int t;
+  cin >> t;
+  string str;
+  for (int i = 0; i < t; i++) {
+    cin >> str;
+    cout << str[0] << str[str.length()-1] << '\n';
+  }
+  return 0;
+}
