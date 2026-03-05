d0ce92f211a8cd09b18c19c9b446ea7bd346ddd5
KimJiHeon <104560192+mulmanmyong@users.noreply.github.com>
2026-03-05 22:26:03 +0900

    [Bronze V] Title: Roller Coaster Ride, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34454. Roller Coaster Ride/Roller Coaster Ride.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N, C, P;
+    cin >> N >> C >> P;
+
+    if (N <= C * P)
+        cout << "yes";
+    else
+        cout << "no";
+
+    return 0;
+}
